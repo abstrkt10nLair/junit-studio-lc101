0ee781b98e338b83edc4a58436cede3356b57d73
Bilal Belagam <bilal.belagam@gmail.com>
2020-07-02 20:39:44 -0400

    wrote some tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,39 @@
 package test;
-
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void oddBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void assertEqualExample() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void returnSpecificString() {
+        assertSame(BalancedBrackets.hasBalancedBrackets("[]"), true);
+    }
+
+    @Test
+    public void doesNotReturnSpecificString() {
+        assertNotSame(BalancedBrackets.hasBalancedBrackets("[]"), false);
+    }
+
+    @Test
+    public void notNullExample() {
+        assertNotNull(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
 
 }
